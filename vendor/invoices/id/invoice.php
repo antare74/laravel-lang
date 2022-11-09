e88f24315db6e495f305f36ec299bc610550818d
antare74 <mc.revs404@gmail.com>
2022-11-09 14:28:53 +0700

    init

---
+++ vendor/invoices/id/invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+    // indonesia
+    'invoice'                => 'INVOICE',
+    'serial'                 => 'Nomor Faktur',
+    'date'                   => 'Tanggal Faktur',
+    'seller'                 => 'Penjual',
+    'buyer'                  => 'Pembeli',
+    'address'                => 'Alamat',
+    'code'                   => 'Kode Pelanggan',
+    'vat'                    => 'NPWP',
+    'phone'                  => 'Telepon',
+    'description'            => 'Deskripsi',
+    'units'                  => 'Satuan',
+    'quantity'               => 'Jumlah',
+    'price'                  => 'Harga',
+    'discount'               => 'Diskon',
+    'tax'                    => 'Pajak',
+    'sub_total'              => 'Sub Total',
+    'total_discount'         => 'Total Diskon',
+    'taxable_amount'         => 'Jumlah Pajak',
+    'total_taxes'            => 'Total Pajak',
+    'tax_rate'               => 'Tarif Pajak',
+    'total_amount'           => 'Total Faktur',
+    'pay_until'              => 'Silahkan membayar sebelum',
+    'amount_in_words'        => 'Jumlah dalam kata',
+    'amount_in_words_format' => '%s %s dan %s %s',
+    'notes'                  => 'Catatan',
+    'shipping'               => 'Pengiriman',
+    'paid'                   => 'Lunas',
+    'due'                    => 'Jatuh Tempo',
+];
